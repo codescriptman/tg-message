5f12dd651f801bd0618a2fc3c482b432d0f79fbc
Azazello <donazazello1312@gmail.com>
2025-09-07 19:59:11 +0300

    dist

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
-// import { getRouter } from "./routers/routers.js";
+import { getRouter } from "./routers/routers.js";
 
 const app = express();
 
 app.use(express.json());
-// app.use("/message", getRouter());
-app.post("*", async (req, res) => {
-  console.log(req.body);
-  res.send("Hello");
-});
+app.use("/message", getRouter());
+// app.post("*", async (req, res) => {
+//   console.log(req.body);
+//   res.send("Hello");
+// });
 
 const PORT = process.env.PORT || 3003;
 
