aeb65844bc9eb2e3d77ce6cb9decb2359bb41f36
Azazello <donazazello1312@gmail.com>
2025-09-07 19:25:24 +0300

    check chat

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import { getRouter } from "./routers/routers.js";
 const app = express();
 
 // app.use(express.json());
-app.use("/message", getRouter());
+// app.use("/message", getRouter());
+app.post("*", async (req, res) => {
+  console.log(req.body);
+  res.send("Hello");
+});
 
 const PORT = process.env.PORT || 3003;
 
