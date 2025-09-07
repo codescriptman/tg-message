bddcc88a7e0c6471e49a034f5de2f9d50c79afcf
Azazello <donazazello1312@gmail.com>
2025-09-07 20:03:57 +0300

    dist

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getRouter } from "./routers/routers.js";
 const app = express();
 
 app.use(express.json());
-app.use("/message", getRouter());
+app.use("/", getRouter());
 // app.post("*", async (req, res) => {
 //   console.log(req.body);
 //   res.send("Hello");
