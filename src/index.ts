735cb153a106ed1a2ea85da256b4e30bd1d3593d
Azazello <donazazello1312@gmail.com>
2025-09-07 20:58:49 +0300

    check

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import express from "express";
-import { getRouter } from "./routers/routers.js";
+// import { getRouter } from "./routers/routers.js";
 import type { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
-app.use("/", getRouter());
+// app.use(bodyParser.json());
+
+app.all("*", (req, res) => {
+  console.log("--- Получен запрос от Telegram ---");
+  console.log(`Метод: ${req.method}`);
+  console.log(`Путь: ${req.path}`);
+  console.log("--- Конец запроса ---");
+  res.sendStatus(200);
+});
+// app.use("/", getRouter());
 // app.post("*", async (req, res) => {
 //   console.log(req.body);
 //   res.send("Hello");
 // });
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send("hello");
-});
+// app.get("*", async (req: Request, res: Response) => {
+//   res.send("hello");
+// });
 
 const PORT = process.env.PORT || 3003;
 
--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -1,18 +1,13 @@
-import { Router } from "express";
-import type { Request, Response } from "express";
-import bodyParser from "body-parser";
+// import { Router } from "express";
+// import type { Request, Response } from "express";
+// import bodyParser from "body-parser";
 
-export const getRouter = () => {
-  const router = Router();
+// export const getRouter = () => {
+//   const router = Router();
 
-  router.post("/", bodyParser.json(), async (req: Request, res: Response) => {
-    //   const newMessage = {
-    //     // from: req.body.from,
-    //     // text: req.body.text,
-    //       // lel: req.body.tel,
-    //   };
-    res.send(req.body);
-  });
+//   router.post("/", bodyParser.json(), async (req: Request, res: Response) => {
+//     res.send(req.body);
+//   });
 
-  return router;
-};
+//   return router;
+// };
